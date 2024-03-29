c645100b6224c876a117213401c0dd32820c1371
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2024-03-29 15:30:28 +0100

    First version of calendar grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar="calendar.pest"]
+pub struct CalendarParser;
 fn main() {
-    println!("Hello, world!");
+    let example_entry = "*-7-2 #extend 1w | Bob's birthday";
+    let parse_tree = CalendarParser::parse(Rule::calendar, example_entry);
+    println!("{:#?}", parse_tree);
 }
