1fe0a8c0c4f3467872304a8fc7e372314d7e38ba
r4ulill0 <36534958+r4ulill0@users.noreply.github.com>
2024-04-01 17:22:32 +0200

    Basic grammar (#1)
    * First version of calendar grammar
    
    * Added some tests and grammar corrections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar="calendar.pest"]
+pub struct CalendarParser;
 fn main() {
-    println!("Hello, world!");
+    let example_entry = "*-7-2 #extend 1w | Bob's birthday";
+    let parse_tree = CalendarParser::parse(Rule::calendar, example_entry);
+    println!("{:#?}", parse_tree);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use crate::{CalendarParser, Rule};
+    use lazy_static::lazy_static;
+    use pest_test::{PestTester, TestError};
+
+    lazy_static! {
+        static ref COLORIZE: bool = {
+                    option_env!("CARGO_TERM_COLOR").unwrap_or("always") != "never"
+                };
+        static ref TESTER: PestTester<Rule, CalendarParser> = 
+        // TODO bug in pest_test 0.1.6
+        PestTester::new("tests/pest","txt", Rule::calendar, HashSet::new());
+        //PestTester::from_defaults(Rule::calendar, HashSet::new());
+    }
+
+    #[test]
+    fn test_every_day_task() -> Result<(), TestError<Rule>> {
+        let res = (*TESTER).evaluate_strict("every_day_task");
+        if let Err(pest_test::TestError::Diff { ref diff }) = res {
+                        diff.print_test_result(*COLORIZE).unwrap();
+                    }
+        res
+    }
+
+    #[test]
+    fn event_early_notification() -> Result<(), TestError<Rule>> {
+        let res = (*TESTER).evaluate_strict("event_early_notification");
+        if let Err(pest_test::TestError::Diff { ref diff }) = res {
+                        diff.print_test_result(*COLORIZE).unwrap();
+                    }
+        res
+    }
+
+    #[test]
+    fn small_calendar() -> Result<(), TestError<Rule>> {
+        let res = (*TESTER).evaluate_strict("small_calendar");
+        if let Err(pest_test::TestError::Diff { ref diff }) = res {
+                        diff.print_test_result(*COLORIZE).unwrap();
+                    }
+        res
+    }
 }
